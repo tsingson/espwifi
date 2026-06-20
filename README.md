c822a194412a2ec72902bd88579d21dad51f9c0a
qinshen <tsingson@gmail.com>
2026-06-20 17:10:44 +0800

    测试通过
    1. macOS 连接上  ESP_wifi_web
    2. curl http://192.168.4.1/get_pin 返回数据
    3.   cmd/bin/cli 运行成功并返回数据

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # espwifi
 esp32 dev module try a wifi web serv
+
+``` 
+连接后，您的 Mac 可以通过浏览器访问 ESP32 的默认 IP http://192.168.4.1/get_pin 来查看 D2 的状态。
+
+```
--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	// 确保将 IP 替换为你 ESP32 在路由器中实际获取到的地址
-	resp, err := http.Get("http://192.168.1.100/get_pin")
+	resp, err := http.Get("http://192.168.4.1/get_pin")
 	if err != nil {
 		fmt.Println("请求失败:", err)
 		return
